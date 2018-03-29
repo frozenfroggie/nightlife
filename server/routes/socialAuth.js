a1fb29bfa5f5038e19d01b800e7ca27b03fef8eb
frozenfroggie <wojtyraj@gmail.com>
2018-03-29 20:54:57 +0200

    github auth issue 2

--- server/routes/socialAuth.js
+++ server/routes/socialAuth.js
@@ -4,11 +4,11 @@ const passport = require('passport');
 
 const axios = require('axios');
 router.get('/github', function(req,res) {
-  res.redirect(`https://github.com/login/oauth/authorize?scope=user:email&client_id=${process.env.CLIENT_ID}&redirect_uri=http://192.168.0.234:8080/auth/github/callback`);
+  res.redirect(`https://github.com/login/oauth/authorize?scope=user:email&client_id=${process.env.CLIENT_ID}&redirect_uri=https://vast-everglades-58513.herokuapp.com/auth/github/callback`);
 });
 router.get('/github/callback', function(req,response) {
   const options = {'headers': {'Accept': 'application/json'}};
-  axios.post(`https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&redirect_uri=http://192.168.0.234:8080/auth/github/callback&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}`, options)
+  axios.post(`https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&redirect_uri=https://vast-everglades-58513.herokuapp.com&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}`, options)
        .then(res => {
            axios.get(`https://api.github.com/user?${res.data}`).then(res => {
              console.log(res.data);
