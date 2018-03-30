7924f8b1311441b17a736e29096acd13345fb54a
frozenfroggie <wojtyraj@gmail.com>
2018-03-30 23:39:07 +0200

    small change to facebook strategy

--- server/passport/strategies/facebookAuth.js
+++ server/passport/strategies/facebookAuth.js
@@ -9,10 +9,11 @@ module.exports = function() {
     passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: "https://vast-everglades-58513.herokuapp.com/socialAuth/facebook/callback"
+    callbackURL: "https://vast-everglades-58513.herokuapp.com/socialAuth/facebook/callback",
+    profileFields: ['id', 'emails', 'name']
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+    console.log('fb', profile);
     const { id, displayName, username, emails } = pick(profile, ['id', 'displayName', 'username', 'emails']);
     FacebookAuthModel.findOrCreate({ id, displayName, username, email: emails[0].value, isVerified: true }, function (err, user) {
                 console.log("logged in");
--- server/routes/socialAuth.js
+++ server/routes/socialAuth.js
@@ -24,7 +24,7 @@ const axios = require('axios');
 // router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 // router.get('/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/' }));
 
-router.get('/facebook', passport.authenticate('facebook'));
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/', failureFlash: true }));
 
 router.get('/github', passport.authenticate('github'));
