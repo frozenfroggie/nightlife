cb1d92496da16c224a3bc144c325e0ad4993622b
frozenfroggie <wojtyraj@gmail.com>
2018-03-30 23:17:06 +0200

    small issue in facebook strategy

--- server/passport/strategies/facebookAuth.js
+++ server/passport/strategies/facebookAuth.js
@@ -8,7 +8,7 @@ module.exports = function() {
     passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: "https://vast-everglades-58513.herokuapp.com/socialAuth/github/callback"
+    callbackURL: "https://vast-everglades-58513.herokuapp.com/socialAuth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     FacebookAuthModel.findOrCreate({ facebookId: profile.id, profile: profile.displayName }, function (err, user) {
