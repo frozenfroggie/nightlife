602a5fa0024e05387fcef674775bb07f0bb64eb3
frozenfroggie <wojtyraj@gmail.com>
2018-03-31 18:15:47 +0200

    social auth added to authenticate middleware

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,26 +1,31 @@
 const User = require('../models/user');
 
 const authenticate = (req, res, next) => {
-  let authToken;
-  try {
-    authToken = req.headers['authorization'].split(' ')[1];
-  } catch(err) {
-    //ignore if token occurs undefined and split can't work
+  if(req.isAuthenticated()) {
+    console.log('social auth');
+    next();
+  } else {
+    let authToken;
+    try {
+      authToken = req.headers['authorization'].split(' ')[1];
+    } catch(err) {
+      //ignore if token occurs undefined and split can't work
+    }
+    User.findByToken(authToken)
+    .then(user => {
+      if(!user) {
+        return Promise.reject();
+      } else {
+        console.log('auth token used');
+        req.user = user;
+        req.authToken = authToken;
+        next();
+      }
+    }).catch(err => {
+      console.log('auth token expired');
+      res.status(401).send({error: 'auth token expired'});
+    });
   }
-  User.findByToken(authToken)
-      .then(user => {
-        if(!user) {
-          return Promise.reject();
-        } else {
-          console.log('auth token used');
-          req.user = user;
-          req.authToken = authToken;
-          next();
-        }
-      }).catch(err => {
-          console.log('auth token expired');
-          res.status(401).send({error: 'auth token expired'});
-      });
 };
 
 module.exports = authenticate;
