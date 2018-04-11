a5a437fb3169f47fd4a9d6f800d903be2463ec01
frozenfroggie <wojtyraj@gmail.com>
2018-04-11 18:16:57 +0200

    Error page styling

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const authenticate = require('../middleware/authenticate');
 
-router.get('/google', passport.authorize('google', { scope: ['profile', 'email'] }));
+router.get('/google', authenticate, passport.authorize('google', { scope: ['profile', 'email'] }));
 
-router.get('/facebook', passport.authorize('facebook', { scope: ['email'] }));
+router.get('/facebook', authenticate, passport.authorize('facebook', { scope: ['email'] }));
 
-router.get('/github', passport.authorize('github'));
+router.get('/github', authenticate, passport.authorize('github'));
 
 module.exports = router;
