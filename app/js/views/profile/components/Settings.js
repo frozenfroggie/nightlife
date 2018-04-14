a1127182ed9d6bff6e2f10ce969b24fea558d5de
frozenfroggie <wojtyraj@gmail.com>
2018-04-14 19:03:56 +0200

    Connect working

--- app/js/views/profile/components/Settings.js
+++ app/js/views/profile/components/Settings.js
@@ -22,21 +22,21 @@ const Settings = props => {
               <Facebook width={style.width} height={style.height} />
               <span> Facebook </span>
               <a href='/socialAuth/facebook'>
-                <div className={classnames(['connect', {'connected': props.user.facebook.id}])}> { props.user.facebook.id ? 'Connected' : 'Connect'} </div>
+                <div className={classnames(['connect', {'connected': props.user.facebook && props.user.facebook.id}])}> { props.user.facebook.id ? 'Connected' : 'Connect'} </div>
               </a>
             </div>
             <div className="socialBtn">
               <Google width={style.width} height={style.height} />
               <span> Google+ </span>
               <a href='/socialAuth/google'>
-                <div className={classnames(['connect', {'connected': props.user.google.id}])}> { props.user.google.id ? 'Connected' : 'Connect'} </div>
+                <div className={classnames(['connect', {'connected': props.user.google && props.user.google.id}])}> { props.user.google.id ? 'Connected' : 'Connect'} </div>
               </a>
             </div>
             <div className="socialBtn">
               <Github width={style.width} height={style.height} />
               <span> Github </span>
               <a href='/socialAuth/github'>
-                <div className={classnames(['connect', {'connected': props.user.github.id}])}> { props.user.github.id ? 'Connected' : 'Connect'} </div>
+                <div className={classnames(['connect', {'connected': props.user.github && props.user.github.id}])}> { props.user.github.id ? 'Connected' : 'Connect'} </div>
               </a>
             </div>
           </div>
