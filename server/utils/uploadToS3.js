ee73832b05139466ec380271153d91cb10437714
frozenfroggie <wojtyraj@gmail.com>
2018-04-23 21:45:12 +0200

    Add generateRandomFileName

--- server/utils/uploadToS3.js
+++ server/utils/uploadToS3.js
@@ -1,15 +1,21 @@
 const aws = require('aws-sdk');
 
+const generateRandomFileName = originalFileName => {
+  const fileNameSplitted = originalFileName.split('.');
+  const fileExt = fileNameSplitted[fileNameSplitted.length - 1];
+  return Date.now().toString() + fileExt;
+}
+
 const uploadToS3 = file => {
   return new Promise((resolve, reject) => {
-    let s3bucket = new aws.S3({
+    const s3bucket = new aws.S3({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_KEY,
       Bucket: process.env.AWS_BUCKET_NAME
     });
-    var params = {
+    const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: Date.now().toString(),
+      Key: generateRandomFileName(file.name),
       Body: file.data
     };
     s3bucket.upload(params, function (err, data) {
