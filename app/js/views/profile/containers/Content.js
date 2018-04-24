64c78cc4e4ba0c07f866c080bb67b8182bb0040f
frozenfroggie <wojtyraj@gmail.com>
2018-04-24 13:48:39 +0200

    Plus styling

--- app/js/views/profile/containers/Content.js
+++ app/js/views/profile/containers/Content.js
@@ -32,7 +32,7 @@ class Content extends React.Component {
             name='avatar'
             onChange={ this.fileSelected }
             ref={ fileInput => this.fileInput = fileInput} />
-          <FontAwesome className='plus' onClick={() => this.fileInput.click()} name='plus' size='2x'/>
+          <FontAwesome className='plus' onClick={() => this.fileInput.click()} name='plus' size='1x'/>
         </div>
         <h2 className="profileUsername">
         {
